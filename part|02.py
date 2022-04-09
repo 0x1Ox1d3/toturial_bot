ebe3667d8e9f38b717c519eefc35e8d8b979b94b
Hamza Hesham <52413136+0x1Ox1d3@users.noreply.github.com>
2022-04-09 23:08:19 +0200

    Create part|02.py

---
+++ part|02.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+import os
+
+
+client = commands.Bot(command_prefix="!")
+
+@client.event
+async def on_ready():
+    print("started")
+
+@client.event
+async def on_message(message):
+    if message == "":
+        return
+    else:
+        id = str(message.author)
+        time = str(message.created_at.strftime("%d-%m-%Y %H:%M:%S"))
+        mss = str(message.content)
+
+        data = open("data.txt","a")
+        data.write(f"({time}) {id} : {mss}\n")
+    
+client.run("Your Token")
